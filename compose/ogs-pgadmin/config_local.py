6463663d7b76d46b334b1a29cdd5146f2373d6f1
Schunicht <volker.schunicht@gov.bc.ca>
2026-01-07 13:00:37 -0800

    tweaks

--- compose/ogs-pgadmin/config_local.py
+++ compose/ogs-pgadmin/config_local.py
@@ -1,5 +1,4 @@
 import os
 
-# Use writable path for SQLite database and session storage
 SQLITE_PATH = os.path.join('/pgadmin4/volumes', 'pgadmin4.db')
 SESSION_DB_PATH = os.path.join('/pgadmin4/volumes', 'sessions.db')
