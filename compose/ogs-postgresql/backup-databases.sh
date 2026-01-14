f8e700ac6c74517fee9055885b8a57f1f90494dc
Volker Schunicht <volker.schunicht@gov.bc.ca>
2026-01-14 13:48:12 -0800

    Modify pg backups

--- compose/ogs-postgresql/backup-databases.sh
+++ compose/ogs-postgresql/backup-databases.sh
@@ -24,10 +24,14 @@ for DB_NAME in "${DATABASES[@]}"; do
     BACKUP_FILE="${BACKUP_DIR}/${DB_NAME}_backup_${DATESTAMP}.dump"
     echo "Backing up database '${DB_NAME}' to '${BACKUP_FILE}'..."
     
-    pg_dump -U "$DB_USER" -d "$DB_NAME" -F c -Z 9 -f "$BACKUP_FILE"
+    pg_dump -U "$DB_USER" -d "$DB_NAME" -F c -Z 9 -f "${BACKUP_FILE}"
     
     if [ $? -eq 0 ]; then
         echo "Backup of '${DB_NAME}' successful."
+		echo "To restore the database to this backup, do the following:"
+		echo "   - log into the OpenShift postgresql pod"
+		echo "   - verify that ${BACKUP_FILE} exists"
+		echo "   - pg_restore -U postgres -d ${DB_NAME} -v ${BACKUP_FILE}"
     else
         echo "Backup of '${DB_NAME}' failed!"
     fi
