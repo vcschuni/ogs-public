385af3883ff01622aff269c5a05a9dcd410f3c05
Volker Schunicht <volker.schunicht@gov.bc.ca>
2026-01-14 12:24:58 -0800

    Update to use specific image

--- build-postgresql.sh
+++ build-postgresql.sh
@@ -34,7 +34,6 @@ echo "========================================"
 echo " Action:            ${ACTION}"
 echo " App:               ${APP}"
 echo " Project:           ${PROJ}"
-echo " Repo:              ${REPO}"
 echo "========================================"
 echo
 read -r -p "Continue? [y/N]: " CONFIRM
@@ -52,8 +51,10 @@ esac
 # Cleanup
 # ----------------------------
 echo ">>> Removing old ${APP} resources..."
-oc delete all -l app="${APP}" --ignore-not-found --wait=true
+[[ "${ACTION}" == "remove" ]] && oc delete service -l app="${APP}" --ignore-not-found --wait=true
+oc delete bc -l app="${APP}" --ignore-not-found --wait=true
 oc delete builds -l app="${APP}" --ignore-not-found --wait=true
+oc delete deployment -l app="${APP}" --ignore-not-found --wait=true
 oc delete is -l app="${APP}" --ignore-not-found --wait=true
 
 # ----------------------------
@@ -70,14 +71,14 @@ fi
 # Create PVC if it doesn't exist
 # ----------------------------
 if ! oc get pvc "${APP}-data" &>/dev/null; then
-    echo ">>> Creating PVC for PostgreSQL data..."
+    echo ">>> Creating PVC for data..."
     oc apply -f - <<EOF
 apiVersion: v1
 kind: PersistentVolumeClaim
 metadata:
-  name: ${APP}-data   
+  name: ${APP}-data
   labels: 
-    app: ${APP}
+    app: ${APP} 
 spec:
   accessModes:
     - ReadWriteOnce
@@ -85,12 +86,11 @@ spec:
     requests:
       storage: ${PVC_SIZE}
 EOF
-	echo ">>> Waiting for PVC for to be ready..."
+	echo ">>> Waiting for PVC to be ready..."
 	COUNT=0
 	while true; do
 		STATUS=$(oc get pvc "${APP}-data" -o jsonpath='{.status.phase}')
 		echo "Current status: $STATUS"
-
 		if [[ "$STATUS" == "Bound" ]]; then
 			echo ">>> PVC is ready!"
 			break
@@ -107,18 +107,47 @@ else
 fi
 
 # ----------------------------
-# Deploy PostgreSQL
+# Import base image
+# Needs to match Dockerfile
 # ----------------------------
-echo ">>> Deploying PostgreSQL..."
-oc new-app "$REPO" \
-  --name="${APP}" \
-  -e POSTGRES_DB=$(oc get secret ogs-postgresql -o jsonpath='{.data.POSTGRESQL_DB}' | base64 --decode) \
-  -e POSTGRES_USER=$(oc get secret ogs-postgresql -o jsonpath='{.data.POSTGRESQL_SUPERUSER_USER}' | base64 --decode) \
-  -e POSTGRES_PASSWORD=$(oc get secret ogs-postgresql -o jsonpath='{.data.POSTGRESQL_SUPERUSER_PASSWORD}' | base64 --decode) \
-  --context-dir="compose/${APP}" \
-  --strategy=docker \
-  --labels=app="${APP}" 
- 
+echo ">>> Import base image..."
+oc import-image postgis:18-3.6 \
+	--from=docker.io/postgis/postgis:18-3.6 \
+	--confirm
+
+# ----------------------------
+# Create the build config
+# ----------------------------
+echo ">>> Creating/updating BuildConfig..."
+oc new-build "$REPO" \
+	--name="${APP}" \
+	--context-dir="compose/${APP}" \
+	--strategy=docker \
+	--labels=app="${APP}"
+
+# ----------------------------
+# Start the build
+# ----------------------------
+echo ">>> Starting build from repo..."
+oc start-build "${APP}" --wait
+
+# ----------------------------
+# Create deployment
+# ----------------------------
+echo ">>> Applying Deployment with new image..."
+oc create deployment "${APP}" \
+    --image="image-registry.openshift-image-registry.svc:5000/${PROJ}/${APP}:latest" \
+    --dry-run=client -o yaml | oc apply -f -
+oc label deployment "${APP}" app="${APP}" --overwrite
+
+# ----------------------------
+# Inject runtime variables
+# ----------------------------
+oc set env deployment/"${APP}" \
+	POSTGRES_DB=$(oc get secret ogs-postgresql -o jsonpath='{.data.POSTGRESQL_DB}' | base64 --decode) \
+	POSTGRES_USER=$(oc get secret ogs-postgresql -o jsonpath='{.data.POSTGRESQL_SUPERUSER_USER}' | base64 --decode) \
+	POSTGRES_PASSWORD=$(oc get secret ogs-postgresql -o jsonpath='{.data.POSTGRESQL_SUPERUSER_PASSWORD}' | base64 --decode)
+
 # ----------------------------
 # Inject secrets
 # ----------------------------
@@ -128,26 +157,27 @@ oc set env deployment/"${APP}" --from=secret/ogs-postgresql
 # Attach PVC
 # ----------------------------
 echo ">>> Attaching PVC..."
-oc set volume deployment/"${APP}" --remove --all --confirm
 oc set volume deployment/"${APP}" \
-  --add \
-  --name="${APP}-data" \
-  --type=pvc \
-  --claim-name="${APP}-data" \
-  --mount-path=/bitnami/postgresql
+    --add \
+	--name="${APP}-data" \
+    --type=pvc \
+    --claim-name="${APP}-data" \
+    --mount-path=/var/lib/postgresql
 
 # ----------------------------
 # Rollout and expose internally
 # ----------------------------
-echo ">>> Waiting for PostgreSQL deployment rollout..."
+echo ">>> Waiting for deployment rollout..."
 oc rollout status deployment/"${APP}" --timeout=300s
 
-echo ">>> Exposing PostgreSQL internally..."
-oc expose deployment "${APP}" \
-  --name="${APP}" \
-  --port=5432 \
-  --dry-run=client -o yaml \
-  --labels=app="${APP}" | oc apply -f -
+if ! oc get service "${APP}" &>/dev/null; then
+	echo ">>> Creating internal service..."
+	oc expose deployment "${APP}" \
+	  --name="${APP}" \
+	  --port=5432 \
+	  --labels=app="${APP}" \
+	  --dry-run=client -o yaml | oc apply -f -
+fi
 
 # ----------------------------
 # Cleanup builds
@@ -157,4 +187,7 @@ oc delete builds -l app="${APP}" --ignore-not-found --wait=true
 # ----------------------------
 # Final status
 # ----------------------------
+echo
 echo ">>> COMPLETE — ${APP} deployed!"
+echo ">>> To rollback: oc rollout undo deployment/${APP}"
+echo
